6c142c5bcd5dde483974d13e37cba0b27adaedca
Juan D. Jara <juanorigami@gmail.com>
2020-12-30 23:58:32 +0100

    fix icon classes

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,9 @@
 export function getClassname ({ hasIcon, outline }) {
   const hoverClasses = 'hover:opacity-75'
   const focusClasses = 'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-  const colorClass = outline ? 'text-white bg-red-900 bg-opacity-50' : 'text-red-900 bg-gray-50'
+  const colorClass = outline ? 'text-white bg-red-900 bg-opacity-50' : 'text-red-900 bg-white'
   const iconSideClass = hasIcon === 'right' ? 'pr-3' : 'pl-3'
-  const contentClass = hasIcon ? `flex align-center space-x-2 ${iconSideClass}` : ''
+  const contentClass = hasIcon ? `flex justify-center items-center space-x-2 ${iconSideClass}` : ''
   const baseClass = 'px-4 py-2 m-2 text-md font-semibold rounded-md border-2 border-current'
   return `${colorClass} ${baseClass} ${contentClass} ${hoverClasses} ${focusClasses}`
 }
