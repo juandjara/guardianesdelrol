c9f31f693d52bb1756c78a386bb9eca9419316da
Juan D. Jara <juanorigami@gmail.com>
2021-01-01 13:06:59 +0100

    more user handling scripts

---
+++ scripts/deleteAnonUsers.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+require('dotenv').config({ path: `${__dirname}/../.env.local` })
+const admin = require('../lib/firebaseAdmin')
+
+async function process () {
+  const ref = await admin.auth().listUsers(500)
+  const ids = ref.users
+    .filter(u => u.providerData.length === 0)
+    .map(u => u.uid)
+  
+  const res = await admin.auth().deleteUsers(ids)
+  console.log(res)
+}
+
+process()
---
+++ scripts/migrateUsers.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+require('dotenv').config({ path: `${__dirname}/../.env.local` })
+const admin = require('../lib/firebaseAdmin')
+const db = admin.firestore()
+
+async function process () {
+  const usersRef = await db.collection('users').get()  
+  const authRef = await admin.auth().listUsers(100)
+
+  // console.log('user emails', usersRef.docs.map(u => u.id).sort())
+  // console.log('auth emails', authRef.users.map(u => u.email).sort())
+
+  for (const user of authRef.users) {
+    const dbuser = usersRef.docs.find(u => u.id === user.email)
+    const { avatarRef = null, displayName = null, photoURL = null, email = null } = dbuser.data()
+    const res = await db.collection('profiles').doc(user.uid).set({
+      acceptingChallenges: false,
+      avatarRef,
+      displayName,
+      photoURL,
+      email: email || dbuser.id || null
+    })
+    console.log(res)
+    // if (dbuser) {
+    //   await admin.auth().updateUser(user.uid, {
+    //     displayName: dbuser.data().displayName,
+    //     photoURL: dbuser.data().photoURL
+    //   })
+    // }
+  }
+}
+
+process()
