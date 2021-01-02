78a30260325c06c40d575005a2179c5dca3df6ea
Juan D. Jara <juanorigami@gmail.com>
2021-01-02 02:35:21 +0100

    add transition to mobile nav icon

--- components/MobileNavMenu.js
+++ components/MobileNavMenu.js
@@ -4,18 +4,19 @@ import MenuIcon from './icons/MenuIcon'
 import CloseIcon from './icons/CloseIcon'
 import { Menu, Transition } from '@headlessui/react'
 
+function TransitionMenuIcon ({ as: Component, show }) {
+  return <Component width={24} height={24} className={`absolute top-1/2 left-1/2 -mt-3 -ml-3 transition duration-500 transform ${show ? '' : 'opacity-0 scale-50'}`} />
+}
+
 export default function MobileNav () {
   return (
     <Menu>
       {({ open }) => (
         <>
-          <Menu.Button className={`${getButtonClassname({ outline: true })} pl-2 pr-2`}>
+          <Menu.Button className={`${getButtonClassname({ outline: true })} relative w-11 h-11`}>
             <span className="sr-only">Mobile nav menu</span>
-            {open ? (
-              <CloseIcon width={24} height={24} />
-            ) : (
-              <MenuIcon width={24} height={24} />
-            )}
+            <TransitionMenuIcon as={CloseIcon} show={open} />
+            <TransitionMenuIcon as={MenuIcon} show={!open} />
           </Menu.Button>
           <Transition
             show={open}
