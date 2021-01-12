1b5912460366332ce475630c10c9807437c4370b
Juan D. Jara <juanorigami@gmail.com>
2021-01-13 00:08:24 +0100

    user detail tweaks

--- components/users/UserDetail.js
+++ components/users/UserDetail.js
@@ -1,8 +1,10 @@
 import useUsers from '@/lib/useUsers'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Skeleton from 'react-loading-skeleton'
 import Avatar from '../Avatar'
 import Button from '../Button'
+import CloseIcon from '../icons/CloseIcon'
 import EditIcon from '../icons/EditIcon'
 import Tag from '../Tag'
 
@@ -21,47 +23,47 @@ export default function UserDetail() {
   return (
     <div className="bg-white w-full md:w-3/4 md:rounded-r-lg border-l border-gray-200 text-gray-700 pb-4">
       <header className="space-x-4 flex items-center py-2 px-4 border-b border-gray-200">
-        <button onClick={() => router.back()} className="rounded-full p-2 hover:bg-gray-100">
-          <svg
-            height={20}
-            width={20}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M10 19l-7-7m0 0l7-7m-7 7h18"
-            />
-          </svg>
-        </button>
+        <Link href="/users">
+          <button className="rounded-full p-2 bg-gray-50 hover:bg-gray-100">
+            <CloseIcon height={20} width={20} />
+          </button>
+        </Link>
         <div className="truncate flex-auto">
-          <p className="font-semibold text-lg">
-            <span>{user?.displayName || <Skeleton />} </span>
-            {user?.role && <Tag>{user.role}</Tag>}
-          </p>
+          <h1 className="font-semibold text-lg">
+            {user ? user.displayName || 'Aventurero sin nombre' : <Skeleton />}
+          </h1>
           <p className="text-sm truncate text-gray-500">
             {aggs ? `${aggs.asPlayer.length} partidas` : <Skeleton />}
           </p>
         </div>
       </header>
-      <main className="mx-4">
-        <div className="-mx-4 h-36 w-auto bg-yellow-500"></div>
-        <div className="flex justify-between">
-          <Avatar className="-mt-12" border="border-white" size={96} user={user} />
+      <section className="mx-4">
+        <div className="-mx-4 h-36 w-auto pattern-bg"></div>
+        <div className="relative md:flex items-center">
+          <div className="-mt-12">
+            <Avatar border="border-gray-100" size={96} user={user} />
+          </div>
+          <div className="mt-2 md:ml-2">
+            <p className="font-semibold text-lg">
+              <span>{user ? user.displayName || 'Aventurero sin nombre' : <Skeleton />} </span>
+              {user?.role && <Tag>{user.role}</Tag>}
+            </p>
+            <p className="mb-2 text-sm truncate text-gray-500">
+              {user ? user.email : <Skeleton />}
+            </p>
+            <p className="text-base">{user ? user.bio : <Skeleton />}</p>
+          </div>
           <Button
+            small
             hasIcon="left"
-            className="rounded-full mx-0"
-            color="text-blue-500 hover:text-white"
-            background="bg-white hover:bg-blue-500 hover:border-blue-500 hover:shadow-md">
+            className="mx-0"
+            color="text-blue-500 w-auto hover:text-white"
+            background="absolute md:top-0 top-12 right-0 bg-white hover:bg-blue-500 hover:border-blue-500 hover:shadow-md">
             <EditIcon width={20} height={20} />
             <span>Editar</span>
           </Button>
         </div>
         <div>
-          <p className="font-semibold text-lg">{user?.displayName || <Skeleton />}</p>
           <p>{aggs ? aggs.gamesPlayed.slice(0, 3).concat('...').join(', ') : <Skeleton />}</p>
           <br />
           <p className="text-base mb-2">
@@ -81,7 +83,7 @@ export default function UserDetail() {
             <span>juegos distintos narrados</span>
           </p>
         </div>
-      </main>
+      </section>
     </div>
   )
 }
