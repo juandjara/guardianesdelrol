02c70bbc88e80e53bf40e986bdd9d40e3175abc9
Juan D. Jara <juanorigami@gmail.com>
2021-01-12 03:07:16 +0100

    images table created

---
+++ scripts/createSupabaseImages.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+require('dotenv').config({ path: `${__dirname}/../.env.local` })
+const images = require('../dump/images.json')
+const supabase = require('../lib/supabaseAdmin')
+
+async function main() {
+  await supabase.from('images').insert(
+    images.map(i => ({
+      name: i.name,
+      type: i.type,
+      size: i.size,
+      download_url: i.downloadUrl,
+      upload_date: i.uploadDate
+    }))
+  )
+
+  console.log('IMAGES INSERTED')
+}
+
+main()
