343e0742368002ecf67492feb90859f4a5a7ce52
Juan D. Jara <juanorigami@gmail.com>
2021-01-12 03:12:05 +0100

    add fio fields to profile fetch

--- db/users_view.sql
+++ db/users_view.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE VIEW users_with_permissions AS 
+DROP VIEW users_with_permissions;
+CREATE VIEW users_with_permissions AS 
   SELECT u.*, p.role FROM public.users u 
   LEFT JOIN public.permissions p
-  ON u.id = p.id
+  ON u.id = p.id;
