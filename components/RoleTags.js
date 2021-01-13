fdba2ba4b4ec015c4596e4f5f2ede32fd554e902
Juan D. Jara <juanorigami@gmail.com>
2021-01-13 11:00:21 +0100

    dev role tag

--- components/RoleTags.js
+++ components/RoleTags.js
@@ -3,7 +3,14 @@ import Tag from './Tag'
 export default function RoleTags({ user }) {
   const role = user?.role
   const tags = []
-  if (role === 'admin' || role === 'superadmin') {
+  if (role === 'superadmin') {
+    tags.push(
+      <Tag color="green" key="dev">
+        dev
+      </Tag>
+    )
+  }
+  if (role === 'admin') {
     tags.push(<Tag key="admin">admin</Tag>)
   }
   // TODO: tag for DM
