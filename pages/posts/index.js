600fdc5ad9884e0034028174d9b955715c43e47b
Juan D. Jara <juanorigami@gmail.com>
2021-01-28 09:58:57 +0100

    extract usePosts hook

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,62 +1,12 @@
 import PostCard from '@/components/PostCard'
-import { supabase } from '@/lib/supabase'
 import useAuthGuard from '@/lib/useAuthGuard'
+import usePosts, { fetchPosts } from '@/lib/usePosts'
 import { useEffect, useRef } from 'react'
-import { useSWRInfinite } from 'swr'
-
-const RPP = 10
-// 0-based pagination
-async function fetcher(key, page = 0) {
-  const { data, error } = await supabase
-    .from('posts')
-    .select(
-      `
-      id,
-      name,
-      slug,
-      tags,
-      seats,
-      date,
-      time,
-      type,
-      image,
-      section(id,name),
-      game(id,name,slug),
-      guest_players,
-      players:users!players(id,email,display_name,avatarType:avatar_type),
-      guest_narrator,
-      narrator:users!narrator(id,email,display_name,avatarType:avatar_type)
-      `
-    )
-    .order('date.desc.nullsfirst,time.desc.nullsfirst,id', { ascending: false, nullsFirst: true })
-    .range(page * RPP, (page + 1) * RPP - 1)
-  if (error) {
-    console.error(error)
-    throw error
-  }
-
-  return data
-}
-
-function getNextPage(page, prevData) {
-  if (prevData && !prevData.length) {
-    return null
-  }
-  if (page === 0) {
-    return ['post-list', null]
-  }
-
-  return ['post-list', page]
-}
 
 export default function PostList({ initialPosts }) {
   useAuthGuard()
   const loaderRef = useRef(null)
-  const { data, isValidating: loading, size: page, setSize: setPage } = useSWRInfinite(
-    getNextPage,
-    fetcher,
-    { initialData: [initialPosts] }
-  )
+  const { data, loading, page, setPage } = usePosts(initialPosts)
 
   useEffect(() => {
     function callback(entries) {
@@ -89,7 +39,7 @@ export default function PostList({ initialPosts }) {
 }
 
 export async function getStaticProps() {
-  const posts = await fetcher(null, 0)
+  const posts = await fetchPosts()
   return {
     props: { initialPosts: posts },
     revalidate: 1
