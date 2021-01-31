6f360c55daaf35d082554d2629393c01566f118e
Juan D. Jara <juanorigami@gmail.com>
2021-01-31 01:18:11 +0100

    tweaks to tags and cards

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,11 +1,23 @@
 import imageKitLoader from '@/lib/imageKitLoader'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRef } from 'react'
 
 export default function GameCard({ game }) {
+  const linkRef = useRef()
   const image = game.posts.find(p => p.image)?.image
+
+  function triggerLink(ev) {
+    const link = linkRef.current
+    if (link && link !== ev.target) link.click()
+  }
+
   return (
-    <li className="flex flex-col md:flex-row bg-white text-gray-700 rounded-lg ring-red-500 hover:ring-4 focus-within:ring-4 transition-shadow duration-500">
-      <div className="h-44 md:w-80 md:flex-shrink-0 relative">
+    <li
+      role="presentation"
+      onClick={triggerLink}
+      className="flex flex-col md:flex-row bg-white text-gray-700 rounded-lg ring-red-500 hover:ring-4 focus-within:ring-4 transition-shadow duration-500">
+      <div className="h-44 md:w-80 md:flex-shrink-0 relative clip-horizontal">
         {image && (
           <Image
             className="rounded-t-lg md:rounded-t-none md:rounded-l-lg"
@@ -17,11 +29,13 @@ export default function GameCard({ game }) {
           />
         )}
       </div>
-      <div className="md:border-l border-gray-200 max-w-prose py-2 px-3 flex flex-col flex-grow justify-center">
-        <h2 className="text-lg font-bold">
-          <a className="text-red-700" href={`/catalog/${game.id}/${game.slug}`}>
-            {game.name}
-          </a>
+      <div className="max-w-prose p-3 flex flex-col flex-grow justify-center">
+        <h2 className="text-lg font-bold truncate text-red-700">
+          <Link href={`/catalog/${game.id}/${game.slug}`}>
+            <a ref={linkRef} className="text-red-700 hover:no-underline">
+              {game.name}
+            </a>
+          </Link>
         </h2>
         <div className="space-y-1">
           <p className="text-sm text-gray-400 mb-1">
--- components/PostCard.js
+++ components/PostCard.js
@@ -1,20 +1,31 @@
 import imageKitLoader from '@/lib/imageKitLoader'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRef } from 'react'
 import Avatar from './Avatar'
 import ClockIcon from './icons/ClockIcon'
 import UserIcon from './icons/UserIcon'
 import Tag from './Tag'
 
 export default function PostCard({ post }) {
-  const tags = [post.type, post.section?.name, ...post.tags].filter(Boolean).map(tag => (
+  const linkRef = useRef()
+  const tags = [post.section?.name, post.type, ...post.tags].filter(Boolean).map(tag => (
     <Tag key={tag} color="red">
       {tag}
     </Tag>
   ))
 
+  function triggerLink(ev) {
+    const link = linkRef.current
+    if (link && link !== ev.target) link.click()
+  }
+
   return (
-    <li className="flex h-96 flex-col bg-white text-gray-700 rounded-lg ring-red-500 hover:ring-4 focus-within:ring-4 transition-shadow duration-500">
-      <div className="h-44 relative">
+    <li
+      role="presentation"
+      onClick={triggerLink}
+      className="flex h-96 flex-col bg-white text-gray-700 rounded-lg ring-red-500 hover:ring-4 focus-within:ring-4 transition-shadow duration-500">
+      <div className="h-44 relative clip-vertical">
         {post.image && (
           <Image
             className="rounded-t-lg"
@@ -27,13 +38,15 @@ export default function PostCard({ post }) {
         )}
       </div>
       <div className="max-w-prose py-2 px-3 flex flex-col flex-grow">
-        <div className="mb-1 space-x-2 flex flex-wrap items-stretch">{tags}</div>
-        <h2 className="text-lg font-bold">
-          <a className="text-red-700" href={`/posts/${post.id}/${post.slug}`}>
-            {post.name}
-          </a>
+        <h2 className="text-lg font-bold truncate text-red-700">
+          <Link href={`/posts/${post.id}/${post.slug}`}>
+            <a ref={linkRef} className="text-red-700 hover:no-underline">
+              {post.name}
+            </a>
+          </Link>
         </h2>
         <p className="text-base text-gray-500 mb-2">{post.game?.name}</p>
+        <div className="space-x-2 flex flex-wrap items-stretch font-semibold mb-2">{tags}</div>
         <div className="mt-auto text-gray-600">
           <p className="flex items-center space-x-2 text-sm mb-2">
             <UserIcon width={20} height={20} />
