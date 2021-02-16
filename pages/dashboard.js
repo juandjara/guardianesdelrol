9fc927368e05b82061a3ad34296f8e083c4e3a61
Juan D. Jara <juanorigami@gmail.com>
2021-02-16 16:24:05 +0100

    add posts stats to dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -11,6 +11,16 @@ export default function Dashboard() {
   const { posts } = usePostsForUser()
 
   const name = (user?.displayName || '').split(' ')[0]
+  let numasdm = 0
+  let numasplayer = 0
+  for (const post of posts) {
+    if (post.narrator.id === user.id) {
+      numasdm++
+    } else {
+      numasplayer++
+    }
+  }
+
   return (
     <main className="max-w-screen-lg w-full mx-auto flex-auto px-4">
       <header className="flex items-center justify-between my-6 mx-1">
@@ -25,14 +35,19 @@ export default function Dashboard() {
       <section className="my-12">
         <header className="flex items-baseline justify-between mx-1">
           <h2 id="myposts" className="text-xl font-semibold">
-            Mis partidas
+            <span>Mis partidas</span>
+            <span className="text-sm ml-2 font-medium">
+              ({numasdm} narradas, {numasplayer} jugadas)
+            </span>
           </h2>
           <Link href="/posts">
             <a className="text-blue-300 font-semibold text-sm">Ver todas</a>
           </Link>
         </header>
         <ul className="grid gap-4 grid-cols-cards mt-2">
-          {posts && posts.map(post => <PostCard key={post.id} post={post} />)}
+          {posts.map(post => (
+            <PostCard key={post.id} post={post} />
+          ))}
         </ul>
       </section>
     </main>
