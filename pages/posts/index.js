d1edecb9d0743dcb55d31f777d9bc6636b024ac3
Juan D. Jara <juanorigami@gmail.com>
2021-02-16 21:27:54 +0100

    search box layout working

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -2,12 +2,52 @@ import PostCard from '@/components/PostCard'
 import Spinner from '@/components/Spinner'
 import useAuthGuard from '@/lib/auth/useAuthGuard'
 import usePosts, { fetchPosts } from '@/lib/data/usePosts'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Tag from '@/components/Tag'
 import Button from '@/components/Button'
 import SearchIcon from '@/components/icons/SearchIcon'
 import FilterIcon from '@/components/icons/FilterIcon'
 import AddIcon from '@/components/icons/AddIcon'
+import CloseIcon from '@/components/icons/CloseIcon'
+
+function SearchBox() {
+  const [open, setOpen] = useState(false)
+
+  const button = (
+    <Button
+      small
+      hasIcon="only"
+      className="m-1"
+      background="bg-transparent hover:bg-red-100 hover:bg-opacity-50"
+      color="text-white"
+      border="border-none"
+      onClick={() => setOpen(!open)}>
+      <SearchIcon width={20} height={20} />
+    </Button>
+  )
+  const input = (
+    <div className="absolute top-0 right-4 shadow-md bg-white text-gray-400 flex items-center my-1.5 rounded-md focus-within:ring-2 focus-within:ring-red-700 focus-within:border-red-700">
+      <SearchIcon className="ml-2" width={16} height={16} />
+      <input
+        id="search"
+        type="search"
+        className="h-7 px-2 text-sm rounded-md placeholder-gray-400 border-none focus:outline-none focus:ring-0"
+        placeholder="Buscar partidas"
+      />
+      <Button
+        small
+        hasIcon="only"
+        background="bg-transparent hover:bg-gray-100"
+        color="text-gray-400"
+        border="border-none"
+        onClick={() => setOpen(!open)}>
+        <CloseIcon width={16} height={16} />
+      </Button>
+    </div>
+  )
+
+  return open ? input : button
+}
 
 export default function PostList({ initialPage }) {
   useAuthGuard()
@@ -39,8 +79,8 @@ export default function PostList({ initialPage }) {
 
   return (
     <main className="flex-auto container mx-auto p-3">
-      <header className="rounded-lg pl-4 pr-3 -mx-3 flex sticky top-0 z-10 bg-red-900 bg-opacity-75">
-        <h1 className="flex items-center text-xl font-semibold space-x-2">
+      <header className="rounded-lg pl-4 pr-3 -mx-3 flex items-start sticky top-0 z-10 bg-red-900 bg-opacity-75">
+        <h1 className="flex h-10 items-center text-xl font-semibold space-x-2">
           {count && <Tag color="red">{count}</Tag>}
           <span>Partidas</span>
         </h1>
@@ -52,17 +92,9 @@ export default function PostList({ initialPage }) {
           background="bg-transparent hover:bg-red-100 hover:bg-opacity-50"
           color="text-white"
           border="border-none">
-          <SearchIcon width={20} height={20} />
-        </Button>
-        <Button
-          small
-          hasIcon="only"
-          className="m-1"
-          background="bg-transparent hover:bg-red-100 hover:bg-opacity-50"
-          color="text-white"
-          border="border-none">
           <FilterIcon width={20} height={20} />
         </Button>
+        <SearchBox />
       </header>
       <Button
         small
