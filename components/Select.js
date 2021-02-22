88ae4519803d73d7b1f5b32497922c12f9f914eb
Juan D. Jara <juanorigami@gmail.com>
2021-02-22 18:57:08 +0100

    extend props for select

--- components/Select.js
+++ components/Select.js
@@ -1,10 +1,18 @@
 import { Listbox, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
 
-export default function Select({ options = [], label = '', selected, onChange = () => {} }) {
+export default function Select({
+  className = '',
+  optionsClassname = '',
+  label = '',
+  selected,
+  options = [],
+  position = 'top',
+  onChange = () => {}
+}) {
   function getBackground(active, opt) {
-    const selected = opt.value === selected?.value
-    if (selected) {
+    const isSelected = opt.value === selected?.value
+    if (isSelected) {
       return 'bg-red-300'
     }
     if (active) {
@@ -20,15 +28,17 @@ export default function Select({ options = [], label = '', selected, onChange = 
     if (i === options.length - 1) {
       return 'rounded-b-md'
     }
+    return ''
   }
 
   return (
     <Listbox value={selected} onChange={onChange}>
       {({ open }) => (
-        <>
+        <div className="relative">
           {label && <Listbox.Label>{label}</Listbox.Label>}
-          <Listbox.Button className="bg-red-100 text-gray-700 py-2 pl-3 pr-2 rounded flex items-center space-x-3">
-            {selected?.label ? (
+          <Listbox.Button
+            className={`${className} bg-red-200 text-gray-700 py-2 pl-3 pr-2 rounded flex items-center justify-between space-x-3`}>
+            {String(selected?.label) ? (
               <span className="font-semibold">{selected.label}</span>
             ) : (
               <span className="font-medium">Selecciona una opcion...</span>
@@ -51,13 +61,13 @@ export default function Select({ options = [], label = '', selected, onChange = 
           </Listbox.Button>
           <Transition
             show={open}
-            enter="transition origin-top ease-out"
+            enter={`transition origin-${position} ease-in-out`}
             enterFrom="transform scale-y-50 opacity-0"
             enterTo="transform scale-y-100 opacity-100"
-            leave="transition origin-top ease-out"
+            leave={`transition origin-${position} ease-in-out`}
             leaveFrom="transform scale-y-100 opacity-100"
             leaveTo="transform scale-y-50 opacity-0">
-            <Listbox.Options className="text-gray-700 mt-2" static>
+            <Listbox.Options className={`${optionsClassname} text-gray-700 mt-2 rounded-md`} static>
               {options.map((opt, i) => (
                 <Listbox.Option as={Fragment} key={opt.value} value={opt} disabled={opt.disabled}>
                   {({ active }) => (
@@ -73,7 +83,7 @@ export default function Select({ options = [], label = '', selected, onChange = 
               ))}
             </Listbox.Options>
           </Transition>
-        </>
+        </div>
       )}
     </Listbox>
   )
