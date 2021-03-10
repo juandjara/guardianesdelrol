cc461bfb1a86cf00b656b44ad536e45353118aba
Juan D. Jara <juanorigami@gmail.com>
2021-03-10 02:28:33 +0100

    rename UserContext to AuthContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout'
 import Alert from '@/components/Alert'
 import { AlertProvider } from '@/components/AlertContext'
-import { UserContextProvider } from '@/lib/auth/UserContext'
+import { AuthProvider } from '@/lib/auth/AuthContext'
 import dynamic from 'next/dynamic'
 import '../styles/globals.css'
 import 'core-js/features/object/from-entries'
@@ -11,13 +11,13 @@ const ProgressBar = dynamic(() => import('@/components/ProgressBar'), { ssr: fal
 function App({ Component, pageProps }) {
   return (
     <AlertProvider>
-      <UserContextProvider>
+      <AuthProvider>
         <Layout>
           <ProgressBar />
           <Alert />
           <Component {...pageProps} />
         </Layout>
-      </UserContextProvider>
+      </AuthProvider>
     </AlertProvider>
   )
 }
