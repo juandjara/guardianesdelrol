2d0ead6f8c1405efca6f10aae724671169a7c19a
Juan D. Jara <juanorigami@gmail.com>
2021-03-10 02:21:46 +0100

    add many more fields to post edit

--- pages/posts/edit/[id].js
+++ pages/posts/edit/[id].js
@@ -1,17 +1,24 @@
 import { useAlert } from '@/components/AlertContext'
+import Autocomplete from '@/components/Autocomplete'
 import BackButton from '@/components/BackButton'
 import Button from '@/components/Button'
 import ImageInput from '@/components/ImageInput'
 import Label from '@/components/Label'
+import Select from 'react-select'
 import Title from '@/components/Title'
 import { supabase } from '@/lib/data/supabase'
+import { fetchGames } from '@/lib/data/useGames'
 import usePostDetail from '@/lib/data/usePostDetail'
+import useSections from '@/lib/data/useSections'
 import { defaultImageState, imageReducer } from '@/lib/imageReducer'
 import uploadImage from '@/lib/uploadImage'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 import { useCallback, useEffect, useReducer, useState } from 'react'
 import { mutate } from 'swr'
+import AvatarList from '@/components/AvatarList'
+import TagsInput from '@/components/TagsInput'
+import { fetchUsers } from '@/lib/data/useUsers'
 
 const inputStyles =
   'w-full h-10 px-3 text-base placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-red-700 focus:border-red-700'
@@ -23,8 +30,64 @@ function defaultFormState(post) {
     description: post?.description || '',
     date: post?.date || '',
     time: post?.time || '',
-    place: post?.place || ''
+    place: post?.place || '',
+    seats: post?.seats || 0,
+    game: post?.game,
+    section: post?.section && { label: post?.section?.name, value: post?.section?.id },
+    tags: (post?.tags || []).map(t => ({ label: t, value: t }))
   }
+}
+
+async function fetchUsersForSelect(query) {
+  const { rows } = await fetchUsers(query)
+  return {
+    rows: rows.map(r => ({
+      ...r,
+      name: r.displayName
+    }))
+  }
+}
+
+function AddUserInput() {
+  const [newUser, setNewUser] = useState(null)
+
+  return (
+    <div className="mt-2 max-w-sm flex items-center">
+      <Autocomplete
+        id="new_player"
+        placeholder="Buscar usuarios..."
+        className="w-64"
+        value={newUser}
+        onChange={setNewUser}
+        fetcher={fetchUsersForSelect}
+      />
+      <Button
+        small
+        type="button"
+        hasIcon="left"
+        background="bg-gray-100"
+        color="text-gray-700"
+        title="Añadir nuevo jugador"
+        border="border-gray-200 border-2 hover:border-gray-300"
+        className="ml-2 pl-2">
+        <svg
+          width={24}
+          height={24}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+        <span>Añadir</span>
+      </Button>
+    </div>
+  )
 }
 
 export default function PostEdit() {
@@ -38,6 +101,9 @@ export default function PostEdit() {
   const [loading, setLoading] = useState(false)
   const [form, setForm] = useState(() => defaultFormState(post))
   const [imageState, dispatch] = useReducer(imageReducer, defaultImageState(post))
+
+  const { sections } = useSections()
+  const sectionOptions = sections.map(s => ({ value: s.id, label: s.name }))
 
   useEffect(() => {
     setForm(defaultFormState(post))
@@ -64,17 +130,17 @@ export default function PostEdit() {
         description: form.description,
         image_position: imageState.position,
         image,
-        date: post?.date,
-        time: post?.time,
-        narrator_id: session.user.id,
+        date: form?.date,
+        time: form?.time,
+        narrator_id: post?.narrator_id || session.user.id,
+        seats: form?.seats,
+        place: form?.place,
+        game: form?.game?.id,
+        section: form?.section?.value,
+        tags: (form?.tags || []).map(t => t.label),
         // TODO
-        tags: post?.tags || [],
-        seats: post?.seats,
-        section: post?.section?.id,
-        type: post?.type,
-        place: post?.place,
-        place_link: post?.place_link,
-        game: post?.game?.id
+        // type: post?.type,
+        place_link: post?.place_link
         // guest_narrator: post?.guest_narrator
       }
       if (id) body.id = id
@@ -117,7 +183,7 @@ export default function PostEdit() {
   }
 
   return (
-    <main className="flex-auto py-3 mx-auto max-w-4xl w-full">
+    <main className="flex-auto py-3 mx-auto max-w-3xl w-full">
       <Title title={title} />
       <header className="flex items-center pl-2 space-x-4">
         <BackButton colors="bg-opacity-20 text-white bg-gray-50 hover:bg-opacity-50" />
@@ -125,12 +191,12 @@ export default function PostEdit() {
       </header>
       <form
         onSubmit={handleSubmit}
-        className="bg-white text-gray-700 space-y-6 mt-2 p-4 pt-6 md:rounded-lg relative">
+        className="bg-white text-gray-700 space-y-8 mt-2 p-4 pt-6 md:rounded-lg relative">
         <div>
           <Label text="Imagen" />
           <ImageInput state={imageState} dispatch={dispatch} />
         </div>
-        <div>
+        <div className="max-w-lg">
           <Label name="name" text="Nombre" />
           <input
             id="name"
@@ -142,19 +208,49 @@ export default function PostEdit() {
             required
           />
         </div>
-        <div className="flex flex-wrap">
-          <div className="flex-grow mb-4 mr-2">
-            <Label name="place" text="Lugar" />
-            <input
-              id="place"
-              type="text"
-              className={inputStyles}
-              placeholder=""
-              value={form.place}
-              onChange={ev => update('place', ev.target.value)}
-            />
-          </div>
-          <div className="flex-grow mb-4 mr-2">
+        <div className="max-w-lg">
+          <Label name="game" text="Juego" />
+          <Autocomplete
+            id="game"
+            placeholder="Seleccione un juego..."
+            value={form.game}
+            onChange={value => update('game', value)}
+            fetcher={fetchGames}
+          />
+        </div>
+        <div className="max-w-lg">
+          <Label text="Evento" />
+          <Select
+            isClearable
+            className="react-select"
+            options={sectionOptions}
+            onChange={ev => update('section', ev)}
+            value={form.section}
+            noOptionsMessage={() => 'Ningún juego para esta búsqueda'}
+            placeholder="Selecciona un evento"
+          />
+        </div>
+        <div className="max-w-lg">
+          <Label text="Etiquetas" />
+          <TagsInput
+            value={form.tags}
+            onChange={ev => update('tags', ev)}
+            placeholder="Escribe una etiqueta y pulsa Enter o Tab"
+          />
+        </div>
+        <div className="max-w-lg">
+          <Label name="place" text="Lugar" />
+          <input
+            id="place"
+            type="text"
+            className={inputStyles}
+            placeholder=""
+            value={form.place}
+            onChange={ev => update('place', ev.target.value)}
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          <div className="col-span-2 md:col-span-1">
             <Label name="date" text="Fecha" />
             <input
               id="date"
@@ -166,7 +262,7 @@ export default function PostEdit() {
               required
             />
           </div>
-          <div className="flex-grow mb-4">
+          <div className="col-span-2 md:col-span-1">
             <Label name="time" text="Hora" />
             <input
               id="time"
@@ -178,7 +274,26 @@ export default function PostEdit() {
             />
           </div>
         </div>
-        <div className="h-full editor-wrapper">
+        <div>
+          <div className="mb-4 flex space-x-2 items-baseline">
+            <Label margin="" text="Jugadores" />
+            <span className="flex-shrink-0">{post?.players.length} /</span>
+            <div className="w-20">
+              <input
+                id="seats"
+                type="text"
+                className={inputStyles}
+                placeholder=""
+                value={form.seats}
+                onChange={ev => update('seats', ev.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <AvatarList users={post?.players} />
+          <AddUserInput />
+        </div>
+        <div className="pt-4 h-full editor-wrapper">
           <Label name="" text="Descripción" />
           <TextEditor value={post?.description} onChange={updateEditor} />
         </div>
