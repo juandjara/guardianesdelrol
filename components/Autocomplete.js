07754d3acaee997932b928606c04c3493f7567fe
Juan D. Jara <juanorigami@gmail.com>
2021-03-10 02:16:38 +0100

    autocomplete component

---
+++ components/Autocomplete.js
@@ -0,0 +1,35 @@
+import AsyncSelect from 'react-select/async'
+
+export default function Autocomplete({
+  className = '',
+  id,
+  placeholder,
+  fetcher,
+  value,
+  onChange = () => {}
+}) {
+  async function loadOptions(q) {
+    const query = `autocomplete?q=${encodeURIComponent(q)}&rpp=5`
+    const data = await fetcher(query)
+    return data?.rows || []
+  }
+
+  return (
+    <AsyncSelect
+      inputId={id}
+      placeholder={placeholder}
+      closeMenuOnSelect
+      cacheOptions
+      defaultOptions
+      isClearable
+      loadOptions={loadOptions}
+      className={`react-select ${className}`}
+      value={value}
+      onChange={onChange}
+      getOptionLabel={opt => opt.name}
+      getOptionValue={opt => opt.id}
+      loadingMessage={() => 'Cargando...'}
+      noOptionsMessage={() => 'Ningún juego para esta búsqueda'}
+    />
+  )
+}
