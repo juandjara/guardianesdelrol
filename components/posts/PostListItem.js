de528b0fc54506a053ad0dd24a71af7c941117bf
Juan D. Jara <juanorigami@gmail.com>
2021-03-19 00:46:17 +0100

    extract PostListItem

---
+++ components/posts/PostListItem.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Avatar from '@/components/Avatar'
+
+export default function PostListItem({ post }) {
+  const numplayers = post.players?.length || 0
+  return (
+    <li key={post.id} className="flex items-center relative">
+      <div className="group">
+        <Avatar border="border-gray-200" user={post.narrator} size={32} />
+        <div className="px-1 transition-opacity duration-300 opacity-0 group-hover:opacity-100 h-0 group-hover:h-auto overflow-hidden absolute left-9 top-2">
+          <div className="px-2 py-1 bg-white rounded-md mb-2 w-full shadow-md">
+            <p className="font-medium text-xs text-gray-500">{post.narrator.display_name}</p>
+          </div>
+        </div>
+      </div>
+      <div className="ml-2">
+        <Link href={`/posts/${post.id}/${post.slug}`}>
+          <a className="text-sm text-gray-700">{post.name}</a>
+        </Link>
+        <p className="text-sm text-gray-400">{numplayers} jugadores</p>
+      </div>
+    </li>
+  )
+}
--- pages/catalog/[...path].js
+++ pages/catalog/[...path].js
@@ -3,7 +3,6 @@ import useGameDetail from '@/lib/games/useGameDetail'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import Skeleton from 'react-loading-skeleton'
-import Avatar from '@/components/Avatar'
 import Link from 'next/link'
 import Button from '@/components/Button'
 import AvatarList from '@/components/AvatarList'
@@ -11,28 +10,7 @@ import FsLightbox from 'fslightbox-react'
 import BackButton from '@/components/BackButton'
 import Title from '@/components/Title'
 import ImageKit from '@/components/ImageKit'
-
-function PostListItem({ post }) {
-  const numplayers = post.players?.length || 0
-  return (
-    <li key={post.id} className="flex items-center relative">
-      <div className="group">
-        <Avatar border="border-gray-200" user={post.narrator} size={32} />
-        <div className="px-1 transition-opacity duration-300 opacity-0 group-hover:opacity-100 h-0 group-hover:h-auto overflow-hidden absolute left-9 top-2">
-          <div className="px-2 py-1 bg-white rounded-md mb-2 w-full shadow-md">
-            <p className="font-medium text-xs text-gray-500">{post.narrator.display_name}</p>
-          </div>
-        </div>
-      </div>
-      <div className="ml-2">
-        <Link href={`/posts/${post.id}/${post.slug}`}>
-          <a className="text-sm text-gray-700">{post.name}</a>
-        </Link>
-        <p className="text-sm text-gray-400">{numplayers} jugadores</p>
-      </div>
-    </li>
-  )
-}
+import PostListItem from '@/components/posts/PostListItem'
 
 export default function GameDetail() {
   // useAuthGuard()
