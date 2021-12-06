f222372db7c5303833f46ad4f0de0094672b861f
Juan D. Jara <juanorigami@gmail.com>
2021-12-06 21:13:48 +0100

    clean users policy sql file

--- db/11_users_policy.sql
+++ db/11_users_policy.sql
@@ -1,13 +1,12 @@
-DROP POLICY IF EXISTS users_policy
-  ON public.users;
+DROP POLICY IF EXISTS users_policy ON public.users;
 
 -- Users Policy:
 -- * only superadmin can write all rows
 -- * any other use can only write their own row
 -- * only logged-in users can read user data
-CREATE POLICY users_policy
-  ON public.users
-  AS PERMISSIVE
+CREATE POLICY users_policy ON public.users AS PERMISSIVE
   FOR ALL
-  USING auth.role() = ANY (ARRAY['superadmin', 'admin', 'authenticated']))
-  WITH CHECK (auth.uid() = id OR auth.role() == 'superadmin');
+    USING auth.role () = ANY (ARRAY['superadmin', 'admin', 'authenticated']))
+    WITH CHECK (auth.uid () = id
+      OR auth.role () = 'superadmin');
+
